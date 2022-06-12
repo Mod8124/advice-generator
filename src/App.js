5121d9925515c69a748ad359b06460d0d0bf2922
denis <denisenriquebaca@gmail.com>
2022-06-12 13:56:43 -0600

    someting

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import AppLogic from './AppLogic';
 function App() { 
 const {advice,handleClick} = AppLogic();
   return (
-    <div className="App">
+    <main className="App">
          {advice &&  <Card advice={advice} handleClick={handleClick}></Card>}
          {!advice && <h2>Loading</h2>}
 
@@ -13,7 +13,7 @@ const {advice,handleClick} = AppLogic();
     Challenge by <a href="https://www.frontendmentor.io?ref=challenge">Frontend Mentor</a>. 
     Coded by <a href="https://mod8124.github.io/portfolio/">Denis</a>.
   </footer>
-    </div>
+    </main>
   );
 }
 
